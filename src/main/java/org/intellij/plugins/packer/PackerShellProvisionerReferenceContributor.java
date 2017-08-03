d083043f87495993b13f191eaad2cc90f2b81aa2
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-08-03 17:12:20 +0300

    Add `{teamplate_dir}}` support in script file references

--- src/main/java/org/intellij/plugins/packer/PackerShellProvisionerReferenceContributor.java
+++ src/main/java/org/intellij/plugins/packer/PackerShellProvisionerReferenceContributor.java
@@ -1,11 +1,11 @@
 package org.intellij.plugins.packer;
 
 import com.intellij.json.psi.*;
-import com.intellij.patterns.PlatformPatterns;
-import com.intellij.patterns.PsiElementPattern;
-import com.intellij.patterns.PsiFilePattern;
+import com.intellij.patterns.*;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceContributor;
 import com.intellij.psi.PsiReferenceRegistrar;
+import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.patterns.XmlPatterns.*;
@@ -17,16 +17,31 @@ public class PackerShellProvisionerReferenceContributor extends PsiReferenceCont
         psiFile(JsonFile.class)
             .inVirtualFile(virtualFile().notNull())
             .withChild(psiElement(JsonObject.class).withChild(psiElement(JsonProperty.class).withName("builders")));
+    private static final ElementPattern<? extends PsiElement> SHELL_PROVISIONER = psiElement(JsonObject.class)
+        .withSuperParent(2, psiElement(JsonProperty.class).withName("provisioners"))
+        .withChild(psiElement(JsonProperty.class).withName("type").with(new PatternCondition<JsonProperty>("JsonPropertyWithStringValue(shell)") {
+            @Override
+            public boolean accepts(@NotNull JsonProperty jsonProperty, ProcessingContext context) {
+                JsonValue value = jsonProperty.getValue();
+                if (value == null) return false;
+                if (!(value instanceof JsonStringLiteral)) return false;
+                JsonStringLiteral literal = (JsonStringLiteral) value;
+                return "shell".equals(literal.getValue());
+            }
+        }));
 
     @Override
     public void registerReferenceProviders(@NotNull final PsiReferenceRegistrar registrar) {
         final PsiElementPattern.Capture<JsonStringLiteral> scripts = PlatformPatterns.psiElement(JsonStringLiteral.class)
             .inFile(PACKER_CONFIG_JSON_PATTERN)
-            .withParent(psiElement(JsonArray.class).withParent(psiElement(JsonProperty.class).withName("scripts")));
+            .withParent(psiElement(JsonArray.class))
+            .withSuperParent(2, psiElement(JsonProperty.class).withName("scripts"))
+            .withSuperParent(3, SHELL_PROVISIONER);
         registrar.registerReferenceProvider(scripts, new PackerShellScriptToFilePsiReferenceProvider());
         final PsiElementPattern.Capture<JsonStringLiteral> script = PlatformPatterns.psiElement(JsonStringLiteral.class)
             .inFile(PACKER_CONFIG_JSON_PATTERN)
-            .withParent(psiElement(JsonProperty.class).withName("script"));
+            .withParent(psiElement(JsonProperty.class).withName("script"))
+            .withSuperParent(2, SHELL_PROVISIONER);
         registrar.registerReferenceProvider(script, new PackerShellScriptToFilePsiReferenceProvider());
     }
 
--- src/main/java/org/intellij/plugins/packer/PackerShellScriptToFilePsiReferenceProvider.java
+++ src/main/java/org/intellij/plugins/packer/PackerShellScriptToFilePsiReferenceProvider.java
@@ -1,14 +1,19 @@
 package org.intellij.plugins.packer;
 
 import com.intellij.json.psi.JsonStringLiteral;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet;
+import com.intellij.util.Function;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+import java.util.Collections;
+
 class PackerShellScriptToFilePsiReferenceProvider extends PsiReferenceProvider {
+
+    private static final String TEMPLATE_DIR_VARIABLE = "{{template_dir}}";
+
     @NotNull
     @Override
     public PsiReference[] getReferencesByElement(@NotNull final PsiElement element, @NotNull final ProcessingContext processingContext) {
@@ -16,18 +21,29 @@ class PackerShellScriptToFilePsiReferenceProvider extends PsiReferenceProvider {
             return PsiReference.EMPTY_ARRAY;
         }
         final JsonStringLiteral literal = (JsonStringLiteral) element;
-//        final PsiFile file = literal.getContainingFile();
-//
-//        if (file == null) {
-//            return PsiReference.EMPTY_ARRAY;
-//        }
 
-        final String script = literal.getValue();
-        return new FileReferenceSet(script, element, 1, null, true) {
+        boolean relativeOnly = false;
+        String script = literal.getValue();
+        int start = 1;
+        if (script.startsWith(TEMPLATE_DIR_VARIABLE)) {
+            script = script.substring(TEMPLATE_DIR_VARIABLE.length());
+            start += TEMPLATE_DIR_VARIABLE.length();
+            relativeOnly = true;
+        }
+        final FileReferenceSet set = new FileReferenceSet(script, element, start, null, true) {
             @Override
             protected boolean isSoft() {
-                return true;
+                return false;
             }
-        }.getAllReferences();
+        };
+        if (relativeOnly) {
+            set.addCustomization(FileReferenceSet.DEFAULT_PATH_EVALUATOR_OPTION, new Function<PsiFile, Collection<PsiFileSystemItem>>() {
+                @Override
+                public Collection<PsiFileSystemItem> fun(PsiFile psiFile) {
+                    return Collections.<PsiFileSystemItem>singleton(psiFile.getParent());
+                }
+            });
+        }
+        return set.getAllReferences();
     }
 }
